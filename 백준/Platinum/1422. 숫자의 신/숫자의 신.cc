1db90af321005a5c6022be23b18e13ab5a784dae
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-02-20 13:40:02 +0900

    [Platinum IV] Title: 숫자의 신, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool cmp(string a, string b) {
+	return a + b < b + a;
+}
+
+int main() {
+	int k, n, i;
+	int maxlen = 0;
+	string str;
+	vector<string> num_arr;
+	cin >> k >> n;
+
+	for (i = 0; i < k; i++) {
+		cin >> str;
+		if (str.length() > maxlen)
+			maxlen = str.length();
+		num_arr.push_back(str);
+	}
+	sort(num_arr.begin(), num_arr.end(),cmp);
+
+	for (i = k - 1; i >= 0; i--) {
+		if (num_arr[i].length() == maxlen) {
+			for (int j = 0; j <= n - k; j++)
+				cout << num_arr[i];
+			i--;
+			break;
+		}
+		cout << num_arr[i];
+	}
+
+	for (i; i >= 0; i--) {
+		cout << num_arr[i];
+	}
+}
