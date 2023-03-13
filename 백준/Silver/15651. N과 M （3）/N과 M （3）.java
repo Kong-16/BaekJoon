6624bb78bee6afdbf472bbca36d4eb131040a372
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-03-13 17:32:32 +0900

    [Silver III] Title: N과 M (3), Time: 604 ms, Memory: 208248 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        // 중복을 허용하므로 처음부터 다 넣어봄.
+        for (int i = 1; i <= n; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        dfs(0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
