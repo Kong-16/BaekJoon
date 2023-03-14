1b089c38e5fc783eb1ddfc14036f4328cbf1ab2e
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-03-15 02:16:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,26 +7,28 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static int n;
-    public static int[] inputArr;
+    public static int[] input;
     public static int[] max;
 
+    // 수열의 모든 수에 대해, 해당 수(x) 를 리스트 max의 가장 마지막 숫자부터 비교.
+    // max[i] < x 인 i 위치 찾음. max[i+1] = x 해줌
+    // => x가 최댓값인 부분수열의 길이가 i임. => 최대 길이의 최솟값 갱신가능.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        inputArr = new int[n];
         max = new int[n + 1];
         max[0] = Integer.MIN_VALUE;
         st = new StringTokenizer(br.readLine());
         int lastidx = 0;
         for (int i = 0; i < n; i++) {
-            inputArr[i] = Integer.parseInt(st.nextToken());
+            int input = Integer.parseInt(st.nextToken());
             int idx = lastidx;
-            while (inputArr[i] <= max[idx]) {
+            while (input <= max[idx]) {
                 idx--;
             }
-            max[idx + 1] = inputArr[i];
+            max[idx + 1] = input;
             if (idx == lastidx)
                 lastidx++;
         }
