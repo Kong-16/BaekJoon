59860e7737a9b6e392724d15f98e91aba57dd4b9
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-03-22 00:57:21 +0900

    [Silver II] Title: 부분수열의 합, Time: 160 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -3,35 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static int n;
     public static int s;
-    public static int[] sequence;
-    public static int[] part;
-    public static boolean[] selected;
+    public static int[] arr;
     public static int ans = 0;
 
-    public static void dfs(int depth) {
-        if (depth == n) {
-            int sum = 0;
-            boolean flag = false;
-            for (int i = 0; i < n; i++) {
-                if (selected[i]) {
-                    flag = true;
-                    sum += sequence[i];
-                }
-            }
-            if (sum == s && flag)
+    public static void dfs(int cur, int total, int cnt) {
+        if (cur == n) {
+            // 모두 고르지 않은 경우 예외 처리
+            if (total == s && 0 < cnt)
                 ans++;
+
             return;
         }
-        selected[depth] = true;
-        dfs(depth + 1);
-        selected[depth] = false;
-        dfs(depth + 1);
+        // arr[cur] 포함 한 경우
+        dfs(cur + 1, total + arr[cur], cnt + 1);
+
+        // arr[cur] 포함 안 한 경우
+        dfs(cur + 1, total, cnt);
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,17 +33,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
-        selected = new boolean[n];
-        sequence = new int[n];
-        part = new int[n];
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            sequence[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0);
+        dfs(0, 0, 0);
+
         bw.write(ans + "\n");
         bw.flush();
         bw.close();
     }
-
 }
