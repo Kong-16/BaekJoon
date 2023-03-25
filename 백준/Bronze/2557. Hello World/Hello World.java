ec1f327093d7ef7cb11ed39ecb44413406d5d39a
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-03-26 03:18:49 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int nearest = Integer.MAX_VALUE;
+    public static boolean[] isBroken;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+
+        bw.flush();
+        bw.close();
+    }
+}
