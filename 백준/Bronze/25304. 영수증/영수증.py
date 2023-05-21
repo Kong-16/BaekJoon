2bd325176f41c2f163601d2d3f659584a93348a9
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2023-05-22 03:13:22 +0900

    [Bronze V] Title: 영수증, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+total = int(input())
+
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    total -= (a * b)
+
+if total == 0:
+    print("Yes")
+else:
+    print("No")
