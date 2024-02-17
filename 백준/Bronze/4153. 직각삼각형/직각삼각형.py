bf6b03ed05092d687b4adb7614062a6997c5df4b
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2024-02-18 01:30:26 +0900

    [Bronze III] Title: 직각삼각형, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    arr = sorted(list(map(int, input().split())))
+    if sum(arr) == 0:
+        break
+    if arr[0] ** 2 + arr[1] ** 2 == arr[2] ** 2:
+        print("right")
+    else:
+        print("wrong")
