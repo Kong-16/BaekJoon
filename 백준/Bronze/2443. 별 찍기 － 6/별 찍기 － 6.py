a71993a2bbb4c58a496f826d17b5d1de98bc6b05
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2024-07-24 03:44:07 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 132 ms, Memory: 110804 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+for i in range(n):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(2 * (n - i) - 1):
+        print("*", end="")
+    print()
