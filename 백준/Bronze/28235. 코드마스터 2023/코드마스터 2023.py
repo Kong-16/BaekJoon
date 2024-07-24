cb550bf632eb28e37ca8edc6c1dcdef9a1713beb
Kong-16 <63219508+Kong-16@users.noreply.github.com>
2024-07-25 01:09:46 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+inp = input().rstrip()
+
+if inp == "SONGDO":
+    print("HIGHSCHOOL")
+elif inp == "CODE":
+    print("MASTER")
+elif inp == "2023":
+    print("0611")
+elif inp == "ALGORITHM":
+    print("CONTEST")
